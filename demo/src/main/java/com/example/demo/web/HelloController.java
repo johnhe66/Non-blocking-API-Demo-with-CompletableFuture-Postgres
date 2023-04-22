d52099fc5079d4e35cbdd54d7d23339dd3f7f116
John He <johnhe53@gmail.com>
2023-04-22 17:07:11 -0400

    create hello web app with spring initializer

---
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    //use flux to create a nonblocking get request handler
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello2";
+    }
+
+}
